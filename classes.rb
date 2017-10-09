79305027ad5670cb939a85dca338c54484ed32e8
jordanberghofer <jordan.berghofer@gmail.com>
2017-10-09 11:43:16 -0500

    Just getting the basic code in the file. Setup basically

---
+++ classes.rb
@@ -0,0 +1,16 @@
+spacer = "********"
+puts spacer
+
+class Company
+    def initialize(comany_name)
+        @comany_name = comany_name
+    end
+
+    # Returns the name of the company
+    def comany_name
+        @comany_name
+    end
+end
+
+#Add the remaining methods to fill the requirements
+
