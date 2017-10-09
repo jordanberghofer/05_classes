9a36bc4fd1bfdbd67e1a9184f7ddf78e708fe460
jordanberghofer <jordan.berghofer@gmail.com>
2017-10-09 15:32:14 -0500

    setting up the two classes I will need.

--- classes.rb
+++ classes.rb
@@ -1,14 +1,26 @@
 spacer = "********"
 puts spacer
 
-class Company
-    def initialize(comany_name)
-        @comany_name = comany_name
+class Customer
+    attr_accessor :account_number
+    attr_accessor :first_name
+    attr_accessor :last_name
+
+    def initialize(an, fn, ln)
+        @account_number = an
+        @first_name = fn
+        @last_name = ln
+    end
+end
+
+class Cool_company
+    def initialize(cool_company)
+        @cool_company = cool_company
     end
 
     # Returns the name of the company
-    def comany_name
-        @comany_name
+    def cool_company
+        @cool_company
     end
 end
 
